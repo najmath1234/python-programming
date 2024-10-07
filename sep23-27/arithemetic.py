24e5b74fcb32f54654ab28da6ceaa924859e01b6
najmath1234 <najumv36@gmail.com>
2024-10-06 20:17:52 -1200

    Add files via upload

---
+++ sep23-27/arithemetic.py
@@ -0,0 +1,10 @@
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+sum=a+b
+difference=a-b
+quotient=a/b
+product=a*b
+print("sum:",sum)
+print("difference:",difference)
+print("quotient:",quotient)
+print("product:",product)
