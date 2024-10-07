9b00dbe7e0e2cc074fbdf18d5d607d5c368425fa
najmath1234 <najumv36@gmail.com>
2024-10-06 21:05:52 -1200

    Add files via upload

---
+++ sep30-oct4/positive negative.py
@@ -0,0 +1,8 @@
+num=int(input("enter a number"))
+if(num>=0):
+    if(num==0):
+        print("number is 0")
+    else:
+            print("number is positive")
+else:
+                print("number is negative")
