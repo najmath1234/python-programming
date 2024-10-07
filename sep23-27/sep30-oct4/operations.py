ee9efb6996365b97a33db748794506ead0e1d1e3
najmath1234 <najumv36@gmail.com>
2024-10-06 20:52:33 -1200

    Add files via upload

---
+++ sep23-27/sep30-oct4/operations.py
@@ -0,0 +1,10 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+sum=a+b
+difference=a-b
+product=a*b
+quotient=a/b
+print("sum:",sum)
+print("difference:",difference)
+print("ptoduct:",product)
+print("quotient:",quotient)
