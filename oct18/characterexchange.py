9182767dc3a38c9261b35413c157a210b50880fc
najmath1234 <najumv36@gmail.com>
2024-10-25 15:38:51 +0530

    Add files via upload

---
+++ oct18/characterexchange.py
@@ -0,0 +1,7 @@
+string=input("enter a string")
+if len(string)>1:
+    modified_string=string[-1]+string[1:-1]+string[0]
+    print(modified_string)
+else:
+    modified_string=string
+    print(modified_string)
