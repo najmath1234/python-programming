e2e418b453fa65542092ed238bb1f9f4f0dec33e
najmath1234 <najumv36@gmail.com>
2025-01-06 15:29:13 +0530

    Add files via upload

---
+++ cuboid.py
@@ -0,0 +1,4 @@
+def cuboid_area(length,breadth,height):
+    return 2 *(length*breadth+length*height+breadth*height)
+def cuboid_perimeter(length,breadth,height):
+    return 4 *(length+breadth+height)
---
+++ sphere.py
@@ -0,0 +1,6 @@
+def sphere_area(radius):
+    from math import pi
+    return 4 * pi * radius * radius
+def sphere_volume(radius):
+    from math import pi
+    return(4/3)*pi*radius*radius*radius
